08b58c3827f274223802029d08ebb579d8c2325d
beingbiplov <sharmabiplov@gmail.com>
2026-02-25 10:48:30 +0100

    feat: Add fullWidth prop to PageShell for flexible layout options

--- components/layout/pageShell.tsx
+++ components/layout/pageShell.tsx
@@ -1,13 +1,13 @@
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 
-export function PageShell({ children }: { children: React.ReactNode }) {
+export function PageShell({ children, fullWidth = false }: { children: React.ReactNode; fullWidth?: boolean }) {
   return (
     <div className='min-h-screen bg-background text-foreground flex flex-col'>
       <Navbar />
 
       <main className='flex-1'>
-        <div className='mx-auto w-full max-w-3xl px-6 py-10'>{children}</div>
+        {fullWidth ? children : <div className='mx-auto w-full max-w-3xl px-6 py-10'>{children}</div>}
       </main>
 
       <Footer />
